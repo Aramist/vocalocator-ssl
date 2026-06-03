d83b9d048ad7820bb154241cb383e1cb24a716b9
Aramis Tanelus <aramis.tanelus@nyu.edu>
2026-06-02 21:01:23 -0400

    Clean up inference routine

--- vocalocatorssl/src/lightning_wrappers.py
+++ vocalocatorssl/src/lightning_wrappers.py
@@ -175,6 +175,7 @@ class LVocalocator(L.LightningModule):
             "predict_calibrate_mode": False,
             "predict_gen_pmfs": False,
             "temperature_adjustment": 1.0,  # For calibration
+            "predict_return_embeddings": False,
         }
         self.entropy_coeff = config["optimization"].get("entropy_coeff", 1.0)
 
@@ -426,10 +427,7 @@ class LVocalocator(L.LightningModule):
 
     def predict_step(
         self, batch: dict[str, torch.Tensor], *args: tp.Any
-    ) -> (
-        tuple[torch.Tensor, torch.Tensor]
-        | tuple[torch.Tensor, torch.Tensor, torch.Tensor]
-    ):
+    ) -> dict[str, torch.Tensor]:
         """Computes score distributions for each candidate source location for each
         sound in the batch.
 
@@ -439,8 +437,11 @@ class LVocalocator(L.LightningModule):
                 locations are expected to have shape (batch, num_negative + 1, num_animals, num_nodes, num_dims)
 
         Returns:
-            torch.Tensor: Labels provided as input
-            torch.Tensor: Scores for each animal (batch, n_animals)
+            Dict with keys:
+            labels: Labels provided as input
+            scores: Scores for each animal (batch, n_animals)
+            pmfs: (Optional) Probability distributions over arena for each sound in the batch (batch, num_theta, num_y, num_x)
+            audio_embeddings: (Optional) Audio embeddings for each sound in the batch (batch, audio_embedding_dim)
         """
         audio = batch["audio"]
         labels = batch["labels"]
@@ -482,12 +483,21 @@ class LVocalocator(L.LightningModule):
         if self.flags["predict_calibrate_mode"]:
             scores = torch.logsumexp(scores, dim=-1)  # sum over animals
 
+        output = {
+            "labels": labels,
+            "scores": scores,
+        }
+
         if self.flags["predict_gen_pmfs"]:
             # Generate PMFs for each animal
             pmfs = self.make_pmf(batch)
-            return labels, scores, pmfs
+            output["pmfs"] = pmfs
 
-        return labels, scores
+        if self.flags["predict_return_embeddings"]:
+            # unexpand audio embeddings to remove redundant data
+            output["audio_embeddings"] = audio_embeddings[:, 0, :]
+
+        return output
 
     def make_pmf(self, batch: dict[str, torch.Tensor]) -> torch.Tensor:
         """Computes score distributions for each candidate source location for each
